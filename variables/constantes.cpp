2e467300ff679ed53ce417288e50fd0b7bbc1dd6
Gustavo <gustavoquintero9105@gmail.com>
2026-05-05 15:35:49 -0500

    Updated on 05-05-2026 at 15:35

---
+++ variables/constantes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double pi = 3.1415926535;
+
+    cout << fixed;
+    cout.precision(2);
+    cout << pi << endl;
+
+    return 0;
+}
