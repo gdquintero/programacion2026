bb26210eb16e2187269bbef22b75a2f650bb3fff
Gustavo <gustavoquintero9105@gmail.com>
2026-05-07 10:39:31 -0500

    Updated on 05-07-2026 at 10:39

---
+++ entrada/potencias.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Type a natural number: ";
+    cin >> x;
+    cout << x % 2 << endl;
+    return 0;
+}
