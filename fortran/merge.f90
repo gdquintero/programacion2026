51776d95213a7dcc46ac29461edda816e2285000
Gustavo <gustavoquintero9105@gmail.com>
2026-06-26 18:01:17 -0500

    Updated on 06-26-2026 at 18:01

---
+++ fortran/merge.f90
@@ -0,0 +1,23 @@
+program merge
+    implicit none
+    
+    real(kind=8), allocatable :: arr(:)
+    real(kind=8) :: star, finish
+    integer :: k, n
+
+    n = 10000
+    arr = [(real(k, kind=8), k = n, 1, -1)]
+
+    ! call cpu_time(star)
+    ! call bubble_sort(arr)
+    ! call cpu_time(finish)
+
+    contains
+
+    subroutine merge_sort(v)
+        implicit none
+
+        real(kind=8),   intent(inout) :: v(:)
+
+    end subroutine merge_sort
+end program merge
